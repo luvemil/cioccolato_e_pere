9d7eb0ab4631163c289db135a363a0c8e1b919b4
luvemil <taran.marco@gmail.com>
2017-11-15 18:45:52 +0100

    Add comments

--- tools.rb
+++ tools.rb
@@ -8,6 +8,16 @@ module BTCData
   @orderbook_template = Addressable::Template.new "https://api.cryptowat.ch/markets{/market}{/pair}{/function}"
 
   def BTCData.get_ohlc options = {}
+    # Populate the api request to get OHLC
+    # options = {
+    #   :market => <market name>
+    #   :pair => <pair name>
+    #   :periods => <string or array of valid periods: 60,180,300,etc>
+    # }
+    # Returns:
+    # {
+    #   <period> => [[CloseTime, OpenPrice, HighPrice, LowPrice, ClosePrice, Volume],...]
+    # }
     api_call = @ohlc_template.partial_expand({
       "function" => "ohlc"
     }).expand(options).to_s
