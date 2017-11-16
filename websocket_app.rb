385057962e1578a6720ede4f51c1314b664dba24
luvemil <taran.marco@gmail.com>
2017-11-16 15:28:27 +0100

    Listen to live feed and save it to file

--- websocket_app.rb
+++ websocket_app.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'json'
 require 'csv'
 require 'dotenv'
@@ -16,20 +17,42 @@ end
 
 client = Bitfinex::Client.new
 
+options = {
+  :market => "bitfinex",
+  :pair => "btcusd",
+}
+
+account_feed = BTCData::FeedSlice.new options[:market], options[:pair], "account", "live_feed"
+ticker_feed = BTCData::FeedSlice.new options[:market], options[:pair], "ticker", "live_feed"
+book_feed = BTCData::FeedSlice.new options[:market], options[:pair], "book", "live_feed"
+trades_feed = BTCData::FeedSlice.new options[:market], options[:pair], "trades", "live_feed"
+
+
 client.listen_account do |c|
-  puts "account: #{c.inspect}"
+  account_feed.append c
 end
 
 client.listen_ticker do |t|
-  puts "tick: #{t.inspect}"
+  ticker_feed.append t
 end
 
 client.listen_book do |b|
-  puts "book: #{b.inspect}"
+  book_feed.append b
 end
 
 client.listen_trades do |b|
-  puts "trades: #{b.inspect}"
+  trades_feed.append b
+end
+
+Config::BOOK_SOURCES.each do |options|
+  new_slice = BTCData::BookSlice.new options[:market], options[:pair]
+  new_slice.read_response BTCData.get_orderbook(options)
+  new_slice.save_csv "live_feed"
+
+  trade_slice = BTCData::TradeSlice.new options[:market], options[:pair]
+  options[:function] = "trades"
+  trade_slice.read_response BTCData.get_function(options)
+  trade_slice.save_csv "live_feed"
 end
 
 client.listen!
