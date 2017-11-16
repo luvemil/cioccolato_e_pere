13c3df69f1f7ec73d3d2118c17948136db9f94cf
luvemil <taran.marco@gmail.com>
2017-11-16 13:04:20 +0100

    Move config to it's own script

--- app.rb
+++ app.rb
@@ -2,39 +2,17 @@ require 'rest-client'
 require 'addressable'
 require 'json'
 require 'csv'
+require 'dotenv'
+
+require 'bitfinex-rb'
+
 require 'tools'
-
-module App
-  DENSITIES = [
-    "60",
-    "300",
-    "3600"
-  ]
-
-  OHLC_SOURCES = [
-    {
-      :market => "bitmex",
-      :pair => "btcusd-perpetual-futures",
-      :periods => DENSITIES
-    },
-    {
-      :market => "bitmex",
-      :pair => "btcusd-quarterly-futures",
-      :periods => DENSITIES
-    },
-    {
-      :market => "bitfinex",
-      :pair => "btcusd",
-      :periods => DENSITIES
-    }
-  ]
-
-end
+require 'config'
 
 if __FILE__ == $0
-  App::OHLC_SOURCES.each do |options|
+  Config::OHLC_SOURCES.each do |options|
     res = BTCData.get_ohlc options
-    App::DENSITIES.each do |density|
+    Config::DENSITIES.each do |density|
       data = res[density]
       outfile = "data/#{options[:market]}_#{options[:pair]}_#{BTCData.date_prefix()}_ohlc_#{density}.csv"
       BTCData.save_csv data, outfile
---
+++ config.rb
@@ -0,0 +1,26 @@
+module Config
+  DENSITIES = [
+    "60",
+    "300",
+    "3600"
+  ]
+
+  OHLC_SOURCES = [
+    {
+      :market => "bitmex",
+      :pair => "btcusd-perpetual-futures",
+      :periods => DENSITIES
+    },
+    {
+      :market => "bitmex",
+      :pair => "btcusd-quarterly-futures",
+      :periods => DENSITIES
+    },
+    {
+      :market => "bitfinex",
+      :pair => "btcusd",
+      :periods => DENSITIES
+    }
+  ]
+
+end
