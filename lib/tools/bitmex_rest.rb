7a4a37bceb8cf517aa519168519665f200281f5c
Marco Tarantino <taran.marco@gmail.com>
2017-12-11 20:43:47 +0100

    Use helper methods

--- lib/tools/bitmex_rest.rb
+++ lib/tools/bitmex_rest.rb
@@ -4,49 +4,62 @@ require 'json'
 
 module BTCData
   module Bitmex
-    @bitmex_api_url = Addressable::Template.new "https://www.bitmex.com/api/v1{/endpoint*}{?params*}"
+    module ApiCall
+      @bitmex_api_url = Addressable::Template.new "https://www.bitmex.com/api/v1{/endpoint*}{?params*}"
+      @density_size = {
+        "m" => 1,
+        "h" => 60,
+        "d" => 60*24,
+        "w" => 60*24*7
+      }
+      def ApiCall._density_to_minutes density
+        m = /(?<qt>\d+)(?<unit>[mhdw])/.match density
+        return nil unless m
+        return m[:qt].to_i * @density_size[m[:unit]]
+      end
 
-    def Bitmex.getOpenInstruments
-      api_call = @bitmex_api_url.expand({
-        "endpoint" => "instrument",
-        "params" => {
-          "filter" => JSON.generate({
-            "state"=>"Open"
-          }),
-          "columns" => ["symbol","rootSymbol"]
-        }
-      }).to_s
+      def ApiCall.openInstruments
+        api_call = @bitmex_api_url.expand({
+          "endpoint" => "instrument",
+          "params" => {
+            "filter" => JSON.generate({
+              "state"=>"Open"
+            }),
+            "columns" => ["symbol","rootSymbol"]
+          }
+        }).to_s
+        return api_call
+      end
+
+      def ApiCall.ohlcDate binSize="5m", symbol="XBTUSD", startTime
+        api_call = @bitmex_api_url.expand({
+          "endpoint" => ["trade","bucketed"],
+          "params" => {
+            "binSize" => binSize,
+            "symbol" => symbol,
+            "count" => @density_size["d"]/ApiCall._density_to_minutes(binSize),
+            "startTime" => startTime.strftime("%Y-%m-%d")
+          }
+        }).to_s
+        return api_call
+      end
+    end
+
+    def Bitmex.callApi api_call
       puts "Trying: #{api_call}"
       res = JSON.parse RestClient.get(api_call)
       return res
+    end
+
+    def Bitmex.getOpenInstruments
+      api_call = ApiCall.openInstruments
+      return Bitmex.callApi api_call
     end
 
     def Bitmex.getOhlcDate binSize="5m", symbol="XBTUSD", startTime
       # startTime : @Time object
-      api_call = @bitmex_api_url.expand({
-        "endpoint" => ["trade","bucketed"],
-        "params" => {
-          "binSize" => binSize,
-          "symbol" => symbol,
-          "count" => @density_size["d"]/Bitmex._density_to_minutes(binSize),
-          "startTime" => startTime.strftime("%Y-%m-%d")
-        }
-      }).to_s
-      puts "Trying: #{api_call}"
-      res = JSON.parse RestClient.get(api_call)
-      return res
-    end
-
-    @density_size = {
-      "m" => 1,
-      "h" => 60,
-      "d" => 60*24,
-      "w" => 60*24*7
-    }
-    def Bitmex._density_to_minutes density
-      m = /(?<qt>\d+)(?<unit>[mhdw])/.match density
-      return nil unless m
-      return m[:qt].to_i * @density_size[m[:unit]]
+      api_call = ApiCall.ohlcDate binSize, symbol, startTime
+      return Bitmex.callApi api_call
     end
 
     def Bitmex._convert_named_cols col_a
@@ -60,5 +73,6 @@ module BTCData
       end
       return data_t.unshift(columns)
     end
+
   end
 end
