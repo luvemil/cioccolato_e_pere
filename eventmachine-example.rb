66d013b05c37aa65eca4b40ce4a35f84bc4cf703
Marco Tarantino <taran.marco@gmail.com>
2017-12-20 16:23:07 +0100

    register to multiple websockets

---
+++ eventmachine-example.rb
@@ -0,0 +1,49 @@
+require 'json'
+require 'eventmachine'
+require 'faye/websocket'
+
+
+@bitfinex = {
+  :url => "wss://api.bitfinex.com/ws/2",
+  :name => "Bitfinex",
+  :open => {
+    "event" => "subscribe",
+    "channel" => "book",
+    "symbol" => "tBTCUSD",
+    "prec" => "P0",
+    "freq" => "F0",
+    "len" => "25"
+  }
+}
+
+@bitmex = {
+  :url => "wss://www.bitmex.com/realtime",
+  :name => "Bitmex",
+  :open => {
+    "op" => "subscribe",
+    "args" => ["orderBookL2"]
+  }
+}
+
+
+EM.run {
+  [@bitmex, @bitfinex].map do |exchange|
+    ws = Faye::WebSocket::Client.new(exchange[:url])
+
+    ws.on :open do |event|
+      p [:open]
+      ws.send(JSON.dump(exchange[:open]))
+    end
+
+    ws.on :message do |event|
+      p [:message, exchange[:name], JSON.parse(event.data)]
+    end
+
+    ws.on :close do |event|
+      p [:close, event.code, event.reason]
+      ws = nil
+    end
+
+    ws
+  end
+}
