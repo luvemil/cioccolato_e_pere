b690f705638558cf5c091e0bc86fec30eb559ba5
luvemil <taran.marco@gmail.com>
2017-12-21 17:56:50 +0100

    Move parser to a separate method

--- eventmachine-example.rb
+++ eventmachine-example.rb
@@ -47,6 +47,34 @@ end
 bitfinex_orderbook_feed = BTCData::FeedSlice.new "bitfinex", "btcusd", "orderbook", live_dir
 bitfinex_orderbook_feed.append %W[Timestamp Price Count Amount]
 
+def log a
+  @count ||= 0
+  EM.stop if @count >= 4
+  p a
+  @count += 1
+end
+
+def bitfinex_parse event, exchange, bitfinex_orderbook_feed, live_dir
+  b = JSON.parse(event.data)
+
+  # START: parsing data
+  data = b[1]
+  if data.kind_of? Array and data.size == 3 and not data.kind_of? String
+    # Case: update
+    bitfinex_orderbook_feed.append data
+  elsif data.kind_of? String
+    # Case: heartbeat
+    p [:message, exchange[:name], b]
+  elsif data.kind_of? Array
+    # Case: snapshot
+    bitfinex_orderbook_snapshot = BTCData::BookSlice.new exchange[:name], "BTCUSD"
+    bitfinex_orderbook_snapshot.data["asks"] = data.select {|x| x[2] < 0}
+    bitfinex_orderbook_snapshot.data["bids"] = data.select {|x| x[2] > 0}
+    bitfinex_orderbook_snapshot.time = bitfinex_orderbook_feed.time
+    bitfinex_orderbook_snapshot.save_csv live_dir
+  end
+end
+
 EM.run {
   #[@bitmex, @bitfinex].each do |exchange|
   [@bitfinex].each do |exchange|
@@ -58,25 +86,7 @@ EM.run {
     end
 
     ws.on :message do |event|
-      b = JSON.parse(event.data)
-      #p [:message, exchange[:name], b]
-
-      # START: parsing data
-      data = b[1]
-      if data.kind_of? Array and data.size == 3 and not data.kind_of? String
-        # Case: update
-        bitfinex_orderbook_feed.append data
-      elsif data.kind_of? String
-        # Case: heartbeat
-        p [:message, exchange[:name], b]
-      elsif data.kind_of? Array
-        # Case: snapshot
-        bitfinex_orderbook_snapshot = BTCData::BookSlice.new exchange[:name], "BTCUSD"
-        bitfinex_orderbook_snapshot.data["asks"] = data.select {|x| x[2] < 0}
-        bitfinex_orderbook_snapshot.data["bids"] = data.select {|x| x[2] > 0}
-        bitfinex_orderbook_snapshot.time = bitfinex_orderbook_feed.time
-        bitfinex_orderbook_snapshot.save_csv live_dir
-      end
+      bitfinex_parse event, exchange, bitfinex_orderbook_feed, live_dir
     end
 
     ws.on :close do |event|
